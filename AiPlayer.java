f7b186e1bd3e92205b7cb124f4e534873d698dad
minsuk2609 <67671019+minsuk2609@users.noreply.github.com>
2021-07-22 16:18:29 -0600

    Finished working on all three of Dealer, HumanPlayer, and AiPlayer class.

---
+++ AiPlayer.java
@@ -0,0 +1,14 @@
+package codes;
+
+public class AiPlayer extends Player{
+
+	public AiPlayer(Deck deck, String name, double startMoney) {
+		super(deck, name, startMoney);
+		this.HUMAN = false;
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+}
---
+++ HumanPlayer.java
@@ -0,0 +1,13 @@
+package codes;
+
+public class HumanPlayer extends Player{
+	public HumanPlayer(Deck deck, String name, double startMoney) {
+		super(deck, name, startMoney);
+		this.HUMAN = true;
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+
+}
